9015af3870b79d3e124d6875050ed9147936c5c7
Evgya <evg.yakub@gmail.com>
2022-06-22 08:49:01 +0300

    Refactor

--- Yandex_training_1.0/1/Сonditioner/Conditioner.py
+++ Yandex_training_1.0/1/Сonditioner/Conditioner.py
@@ -1,10 +1,12 @@
+def get_state_t(troom, tcond, mode):
+    cond = {
+       "freeze": min(troom, tcond),
+       "heat":   max(troom, tcond),
+       "auto":   tcond,
+       "fan":    troom
+    }
+    return cond[mode]
+
 troom, tcond = map(int, input().split())
 mode = input()
-cond = {
-         "freeze": min(troom, tcond),
-         "heat"  : max(troom, tcond),
-         "auto"  : tcond,
-         "fan"   : troom
-}
-
-print(cond[mode])
+print(get_state_t(troom, tcond, mode))
