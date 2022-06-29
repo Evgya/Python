c0040807bdcab64d4140255041f1df2f3b5744b3
Evgya <evg.yakub@gmail.com>
2022-06-29 10:46:55 +0300

    added task Is list increase?

---
+++ Yandex_training_1.0/2/A. is list increase/is_list_increase.py
@@ -0,0 +1,8 @@
+def is_list_increase(_list):
+    return all((x < y for x, y in zip(_list, _list[1:])))
+
+_list = tuple(map(int, input().split()))
+if is_list_increase(_list):
+    print("YES")
+else:
+    print("NO")
