12331cf3e4a04e8861cbdd7403e2c1d3216c9fb7
Evgya <evg.yakub@gmail.com>
2022-07-15 21:18:17 +0300

    added task Difficult_equation

--- Yandex_training_2.0/A/1/A. Difficult_equation/Difficult_equation.py
+++ Yandex_training_2.0/A/1/A. Difficult_equation/Difficult_equation.py
@@ -1,16 +1,16 @@
 def solve_in_int(a, b, c, d):
     if a == 0:
-            if any((b != 0,all((b == 0, c == 0, d == 0)))):
+            if any((b != 0, all((b == 0, c == 0, d == 0)))):
                 return "NO"
             else:
                 return "INF"
-    else:           
-         x =  - b/a 
+    else:
+        x = -b/a
     if x.is_integer():
         x = int(x)
     else:
         return "NO"
-    if ( c*x + d != 0 ):
+    if (c*x + d != 0):
         return x
     return "NO"
 
