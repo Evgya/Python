2c7c5b8b1022cfc9279a2d3de548ce927c112136
Evgya <evg.yakub@gmail.com>
2022-07-15 21:14:10 +0300

    added task Parallelogram

---
+++ Yandex_training_2.0/A/1/B. Parallelogram/Parallelogram.py
@@ -0,0 +1,20 @@
+def is_parallelogram (points):
+    middle_points = set()
+    for i in range(0, len(points), 2):
+        for j in range(i + 2, len(points), 2):
+            if points[i] == points[j] and points[i + 1] == points[j + 1]:
+                return False
+            middle_point = tuple((points[i] + points[j], points[i + 1] + points[j + 1]))            
+            middle_points.add(middle_point)
+    if len(middle_points) == 5:
+        return True
+    return False
+
+N = int(input())
+for i in range (N):
+    points = tuple(map(int, input().split()))    
+    if is_parallelogram(points):
+        print ("YES")
+    else:
+        print("NO")
+ 
