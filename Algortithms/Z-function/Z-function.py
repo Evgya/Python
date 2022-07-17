7dea341b66973726b9a221a980c76c595da8a3bf
Evgya <evg.yakub@gmail.com>
2022-07-17 08:37:02 +0300

    added Z - function

--- Algortithms/Z-function/Z-function.py
+++ Algortithms/Z-function/Z-function.py
@@ -1,9 +1,9 @@
-def calc_z_function(string, N):
-    z = [0] * N
+def calc_z_function(string):
+    z = [0] * len(string)
     left, right = 0, 0
-    for i in range(N):
+    for i in range(1, len(string)):
         z[i] = max(0, min(z[i - left], right - i))
-        while i + z[i] < N and string[z[i]] == string[i + z[i]]:        
+        while i + z[i] < len(string) and string[z[i]] == string[i + z[i]]:        
             z[i] += 1
         if i + z[i] > right:
             left, right = i, i + z[i]
