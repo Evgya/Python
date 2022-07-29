f0ff0c809e6693048a1fb16892bbae9777a7877b
Evgya <evg.yakub@gmail.com>
2022-07-29 11:48:20 +0300

    added task Keyboard

--- Yandex_training_1.0/4/D. Keyboard/Keyboard.py
+++ Yandex_training_1.0/4/D. Keyboard/Keyboard.py
@@ -4,7 +4,7 @@ k = int(input())
 P = tuple(map(int, input().split()))
 _dict = dict()
 for button in P:
-    _dict.update({button: _dict.get(button, 0) + 1})
+    _dict[button] = _dict.setdefault(button, 0) + 1
 for idx in range(1, n + 1):
     if C[idx - 1] - _dict.get(idx, 0) < 0:
         print("YES")
