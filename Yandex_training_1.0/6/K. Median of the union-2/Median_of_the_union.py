81cd7e0006d7d78dc732277ae1077cae95a24b36
Evgya <evg.yakub@gmail.com>
2022-09-11 19:52:00 +0300

    added task Median of the union-2

---
+++ Yandex_training_1.0/6/K. Median of the union-2/Median_of_the_union.py
@@ -0,0 +1,52 @@
+def generate_sequence(x1, d1, a, c, m, L):
+    seq = [x1]
+    d = d1
+    for _ in range(L - 1):
+        seq.append(seq[-1] + d)
+        d = (a*d + c) % m
+    return seq
+
+
+def count_greater(x, _list):
+    return len(_list) - count_less(x + 1, _list)
+
+
+def count_less(x, _list):
+    left_border = 0
+    right_border = len(_list) - 1
+    while left_border < right_border:
+        middle = (left_border + right_border)//2
+        if _list[middle] >= x:
+            right_border = middle
+        else:
+            left_border = middle + 1
+    if _list[left_border] < x:
+        return len(_list)
+    return left_border
+
+
+def get_median_of_merge(seq1, seq2):
+    left_border = min(seq1[0], seq2[0])
+    right_border = max(seq1[-1], seq2[-1])
+    while left_border < right_border:
+        mid = (left_border + right_border)//2
+        less_mid = count_less(mid, seq1) + count_less(mid, seq2)
+        greater_mid = count_greater(mid, seq1) + count_greater(mid, seq2)
+        if less_mid <= len(seq1) - 1 and greater_mid <= len(seq1):
+            return mid
+        if greater_mid > len(seq1):
+            left_border = mid + 1
+        if less_mid > len(seq1) - 1:
+            right_border = mid - 1
+    return left_border
+
+
+N, L = map(int, input().split())
+sequences = []
+for _ in range(N):
+    x1, d1, a, c, m = map(int, input().split())
+    sequences.append(generate_sequence(x1, d1, a, c, m, L))
+
+for i in range(N):
+    for j in range(i + 1, N):
+        print(get_median_of_merge(sequences[i], sequences[j]))
